327d63274237c7093498dbbbde7bafe033f5a1bf
anudeep <anud7781@gmail.com>
2025-12-10 14:44:11 +0530

    Third Correction

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,11 +23,11 @@ app.use(cors({
 app.use("/api/auth", authRouter);
 app.use("/api/diary", diaryRouter);
 
-if(process.env.NODE_ENV==="production"){
+if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
 
-    app.get('(*)',(req,res) => {
+    app.get(/(.*)/, (req,res) => {
         res.sendFile(path.join(__dirname,"../frontend", "dist" ,"index.html"));
     })
 }
